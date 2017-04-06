15189085e48b62767729d70ff9bcc9c9ca5df480
Augus <493912218@qq.com>
2017-04-06 21:53:06 +0800

    增加LogUtils初步将log信息写入文件功能。后续:1、将写入文件模块放在一个线程中执行，2、先内存中缓存一定的信息再一次写入，3、处理多线程调用时候可能发生并发异常

--- LogUtils.java
+++ LogUtils.java
@@ -1,161 +1,237 @@
-package com.utils;
+package Utils;
 
 /**
  * Created by lixing on 2016/6/23.
  */
 
-    import android.text.TextUtils;
-    import android.util.Log;
+import android.os.Environment;
+import android.text.TextUtils;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * LogUtils工具说明:
+ * 1 只输出等级大于等于LEVEL的日志
+ * 所以在开发和产品发布后通过修改LEVEL来选择性输出日志.
+ * 当LEVEL=NOTHING则屏蔽了所有的日志.
+ * 2 v,d,i,w,e均对应两个方法.
+ * 若不设置TAG或者TAG为空则为设置默认TAG
+ */
+public class LogUtils {
+    //log的目录
+    private static final String LOGFILEDIR = "/myapplication/logs/";
+
+    public static final int VERBOSE = 1;
+    public static final int DEBUG = 2;
+    public static final int INFO = 3;
+    public static final int WARN = 4;
+    public static final int ERROR = 5;
+    public static final int NOTHING = 6;
+    public static final int LEVEL = VERBOSE;
+    public static final String SEPARATOR = ",";
+
+    public static void v(String message) {
+        if (LEVEL <= VERBOSE) {
+            StackTraceElement stackTraceElement = Thread.currentThread().getStackTrace()[3];
+            String tag = getDefaultTag(stackTraceElement);
+            Log.v(tag, getLogInfo(stackTraceElement) + message);
+        }
+    }
+
+    public static void v(String tag, String message) {
+        if (LEVEL <= VERBOSE) {
+            StackTraceElement stackTraceElement = Thread.currentThread().getStackTrace()[3];
+            if (TextUtils.isEmpty(tag)) {
+                tag = getDefaultTag(stackTraceElement);
+            }
+            Log.v(tag, getLogInfo(stackTraceElement) + message);
+        }
+    }
+
+    public static void d(String message) {
+        if (LEVEL <= DEBUG) {
+            StackTraceElement stackTraceElement = Thread.currentThread().getStackTrace()[3];
+            String tag = getDefaultTag(stackTraceElement);
+            Log.d(tag, getLogInfo(stackTraceElement) + message);
+        }
+    }
+
+    public static void d(String tag, String message) {
+        if (LEVEL <= DEBUG) {
+            StackTraceElement stackTraceElement = Thread.currentThread().getStackTrace()[3];
+            if (TextUtils.isEmpty(tag)) {
+                tag = getDefaultTag(stackTraceElement);
+            }
+            Log.d(tag, getLogInfo(stackTraceElement) + message);
+        }
+    }
+
+    public static void d(boolean saveLog, String tag, String message){
+        d(tag, message);
+        if(saveLog){
+            saveFile(tag,message);
+        }
+    }
+
+    public static void i(String message) {
+        if (LEVEL <= INFO) {
+            StackTraceElement stackTraceElement = Thread.currentThread().getStackTrace()[3];
+            String tag = getDefaultTag(stackTraceElement);
+            Log.i(tag, getLogInfo(stackTraceElement) + message);
+        }
+    }
+
+    public static void i(String tag, String message) {
+        if (LEVEL <= INFO) {
+            StackTraceElement stackTraceElement = Thread.currentThread().getStackTrace()[3];
+            if (TextUtils.isEmpty(tag)) {
+                tag = getDefaultTag(stackTraceElement);
+            }
+            Log.i(tag, getLogInfo(stackTraceElement) + message);
+        }
+    }
+
+    public static void w(String message) {
+        if (LEVEL <= WARN) {
+            StackTraceElement stackTraceElement = Thread.currentThread().getStackTrace()[3];
+            String tag = getDefaultTag(stackTraceElement);
+            Log.w(tag, getLogInfo(stackTraceElement) + message);
+        }
+    }
+
+    public static void w(String tag, String message) {
+        if (LEVEL <= WARN) {
+            StackTraceElement stackTraceElement = Thread.currentThread().getStackTrace()[3];
+            if (TextUtils.isEmpty(tag)) {
+                tag = getDefaultTag(stackTraceElement);
+            }
+            Log.w(tag, getLogInfo(stackTraceElement) + message);
+        }
+    }
+
+    public static void e(String message) {
+        if (LEVEL <= ERROR) {
+            StackTraceElement stackTraceElement = Thread.currentThread().getStackTrace()[3];
+            String tag = getDefaultTag(stackTraceElement);
+            Log.e(tag, getLogInfo(stackTraceElement) + message);
+        }
+    }
+
+    public static void e(String tag, String message) {
+        if (LEVEL <= ERROR) {
+            StackTraceElement stackTraceElement = Thread.currentThread().getStackTrace()[3];
+            if (TextUtils.isEmpty(tag)) {
+                tag = getDefaultTag(stackTraceElement);
+            }
+            Log.e(tag, getLogInfo(stackTraceElement) + message);
+        }
+    }
 
     /**
-     * LogUtils工具说明:
-     * 1 只输出等级大于等于LEVEL的日志
-     *   所以在开发和产品发布后通过修改LEVEL来选择性输出日志.
-     *   当LEVEL=NOTHING则屏蔽了所有的日志.
-     * 2 v,d,i,w,e均对应两个方法.
-     *   若不设置TAG或者TAG为空则为设置默认TAG
-     *
+     * 获取默认的TAG名称.
+     * 比如在MainActivity.java中调用了日志输出.
+     * 则TAG为MainActivity
      */
-public class LogUtils {
-        public static final int VERBOSE = 1;
-        public static final int DEBUG = 2;
-        public static final int INFO = 3;
-        public static final int WARN = 4;
-        public static final int ERROR = 5;
-        public static final int NOTHING = 6;
-        public static final int LEVEL = VERBOSE;
-        public static final String SEPARATOR = ",";
+    public static String getDefaultTag(StackTraceElement stackTraceElement) {
+        String fileName = stackTraceElement.getFileName();
+        String stringArray[] = fileName.split("\\.");
+        String tag = stringArray[0];
+        return tag;
+    }
 
-        public static void v(String message) {
-            if (LEVEL <= VERBOSE) {
-                StackTraceElement stackTraceElement = Thread.currentThread().getStackTrace()[3];
-                String tag = getDefaultTag(stackTraceElement);
-                Log.v(tag, getLogInfo(stackTraceElement) + message);
+    /**
+     * 输出日志所包含的信息
+     */
+    public static String getLogInfo(StackTraceElement stackTraceElement) {
+        StringBuilder logInfoStringBuilder = new StringBuilder();
+        // 获取线程名
+        String threadName = Thread.currentThread().getName();
+        // 获取线程ID
+        long threadID = Thread.currentThread().getId();
+        // 获取文件名.即xxx.java
+        String fileName = stackTraceElement.getFileName();
+        // 获取类名.即包名+类名
+        String className = stackTraceElement.getClassName();
+        // 获取方法名称
+        String methodName = stackTraceElement.getMethodName();
+        // 获取生日输出行数
+        int lineNumber = stackTraceElement.getLineNumber();
+
+        logInfoStringBuilder.append("[ ");
+        logInfoStringBuilder.append("threadID=" + threadID).append(SEPARATOR);
+        logInfoStringBuilder.append("threadName=" + threadName).append(SEPARATOR);
+        logInfoStringBuilder.append("fileName=" + fileName).append(SEPARATOR);
+        logInfoStringBuilder.append("className=" + className).append(SEPARATOR);
+        logInfoStringBuilder.append("methodName=" + methodName).append(SEPARATOR);
+        logInfoStringBuilder.append("lineNumber=" + lineNumber);
+        logInfoStringBuilder.append(" ] ");
+        return logInfoStringBuilder.toString();
+    }
+
+
+    /**
+     * 获取时间
+     *
+     * @return
+     */
+    private static String getCurrentTime() {
+        long currentTime = System.currentTimeMillis();                    //得到的是ms
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd-HH:mm:ss");
+        Date date = new Date(currentTime);
+        String time = formatter.format(date);
+        return time;
+    }
+
+    /**
+     * 获取log文件绝对路径
+     * @return
+     */
+    private static String getLogFilePath(){
+        String sdcardPath = Environment.getExternalStorageDirectory().getAbsolutePath();
+        String logDir = sdcardPath + LOGFILEDIR;
+        File dir = new File(logDir);
+        if(!dir.exists()){
+            dir.mkdirs();
+        }
+        String logName = "log.log";
+        return sdcardPath + LOGFILEDIR + logName;
+    }
+
+    /**
+     * 将log信息保存到文件中
+     * @param tag
+     * @param msg
+     */
+    private static void saveFile(String tag, String msg) {
+        String time = getCurrentTime();
+        StringBuffer stringBuffer = new StringBuffer();
+        stringBuffer.append(time + "  ").append(tag + ":").append(msg);
+        String content = stringBuffer.toString() + "\n";
+
+        String logPath = getLogFilePath();
+        File logFile = new File(logPath);
+        try {
+            if(!logFile.exists()){
+                logFile.createNewFile();
             }
+            FileOutputStream outputStream = new FileOutputStream(logFile,true);
+            byte[] bytes = content.getBytes();
+            outputStream.write(bytes);
+            outputStream.flush();
+            outputStream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }catch (IOException e) {
+            e.printStackTrace();
         }
 
-        public static void v(String tag, String message) {
-            if (LEVEL <= VERBOSE) {
-                StackTraceElement stackTraceElement = Thread.currentThread().getStackTrace()[3];
-                if (TextUtils.isEmpty(tag)) {
-                    tag = getDefaultTag(stackTraceElement);
-                }
-                Log.v(tag, getLogInfo(stackTraceElement) + message);
-            }
-        }
-
-        public static void d(String message) {
-            if (LEVEL <= DEBUG) {
-                StackTraceElement stackTraceElement = Thread.currentThread().getStackTrace()[3];
-                String tag = getDefaultTag(stackTraceElement);
-                Log.d(tag, getLogInfo(stackTraceElement) + message);
-            }
-        }
-
-        public static void d(String tag, String message) {
-            if (LEVEL <= DEBUG) {
-                StackTraceElement stackTraceElement = Thread.currentThread().getStackTrace()[3];
-                if (TextUtils.isEmpty(tag)) {
-                    tag = getDefaultTag(stackTraceElement);
-                }
-                Log.d(tag, getLogInfo(stackTraceElement) + message);
-            }
-        }
-
-        public static void i(String message) {
-            if (LEVEL <= INFO) {
-                StackTraceElement stackTraceElement = Thread.currentThread().getStackTrace()[3];
-                String tag = getDefaultTag(stackTraceElement);
-                Log.i(tag, getLogInfo(stackTraceElement) + message);
-            }
-        }
-
-        public static void i(String tag, String message) {
-            if (LEVEL <= INFO) {
-                StackTraceElement stackTraceElement = Thread.currentThread().getStackTrace()[3];
-                if (TextUtils.isEmpty(tag)) {
-                    tag = getDefaultTag(stackTraceElement);
-                }
-                Log.i(tag, getLogInfo(stackTraceElement) + message);
-            }
-        }
-
-        public static void w(String message) {
-            if (LEVEL <= WARN) {
-                StackTraceElement stackTraceElement = Thread.currentThread().getStackTrace()[3];
-                String tag = getDefaultTag(stackTraceElement);
-                Log.w(tag, getLogInfo(stackTraceElement) + message);
-            }
-        }
-
-        public static void w(String tag, String message) {
-            if (LEVEL <= WARN) {
-                StackTraceElement stackTraceElement = Thread.currentThread().getStackTrace()[3];
-                if (TextUtils.isEmpty(tag)) {
-                    tag = getDefaultTag(stackTraceElement);
-                }
-                Log.w(tag, getLogInfo(stackTraceElement) + message);
-            }
-        }
-
-        public static void e(String message) {
-            if (LEVEL <= ERROR) {
-                StackTraceElement stackTraceElement = Thread.currentThread().getStackTrace()[3];
-                String tag = getDefaultTag(stackTraceElement);
-                Log.e(tag, getLogInfo(stackTraceElement) + message);
-            }
-        }
-
-        public static void e(String tag, String message) {
-            if (LEVEL <= ERROR) {
-                StackTraceElement stackTraceElement = Thread.currentThread().getStackTrace()[3];
-                if (TextUtils.isEmpty(tag)) {
-                    tag = getDefaultTag(stackTraceElement);
-                }
-                Log.e(tag, getLogInfo(stackTraceElement) + message);
-            }
-        }
-
-        /**
-         * 获取默认的TAG名称.
-         * 比如在MainActivity.java中调用了日志输出.
-         * 则TAG为MainActivity
-         */
-        public static String getDefaultTag(StackTraceElement stackTraceElement) {
-            String fileName = stackTraceElement.getFileName();
-            String stringArray[] = fileName.split("\\.");
-            String tag = stringArray[0];
-            return tag;
-        }
-
-        /**
-         * 输出日志所包含的信息
-         */
-        public static String getLogInfo(StackTraceElement stackTraceElement) {
-            StringBuilder logInfoStringBuilder = new StringBuilder();
-            // 获取线程名
-            String threadName = Thread.currentThread().getName();
-            // 获取线程ID
-            long threadID = Thread.currentThread().getId();
-            // 获取文件名.即xxx.java
-            String fileName = stackTraceElement.getFileName();
-            // 获取类名.即包名+类名
-            String className = stackTraceElement.getClassName();
-            // 获取方法名称
-            String methodName = stackTraceElement.getMethodName();
-            // 获取生日输出行数
-            int lineNumber = stackTraceElement.getLineNumber();
-
-            logInfoStringBuilder.append("[ ");
-            logInfoStringBuilder.append("threadID=" + threadID).append(SEPARATOR);
-            logInfoStringBuilder.append("threadName=" + threadName).append(SEPARATOR);
-            logInfoStringBuilder.append("fileName=" + fileName).append(SEPARATOR);
-            logInfoStringBuilder.append("className=" + className).append(SEPARATOR);
-            logInfoStringBuilder.append("methodName=" + methodName).append(SEPARATOR);
-            logInfoStringBuilder.append("lineNumber=" + lineNumber);
-            logInfoStringBuilder.append(" ] ");
-            return logInfoStringBuilder.toString();
-        }
-
+    }
 
 }
