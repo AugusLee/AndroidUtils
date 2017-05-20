c60418ae17e8a357f0bfa75521c67d4c9b591fba
Augus <493912218@qq.com>
2017-05-20 21:31:50 +0800

    测试Git分支切换

--- CrashHandler.java
+++ CrashHandler.java
@@ -26,7 +26,7 @@ import java.util.Map;
 /**
  * UncaughtException处理类,当程序发生Uncaught异常的时候,有该类来接管程序,并记录发送错误报告. 
  *
- * @author user
+ * @author Augus
  *
  */
 public class CrashHandler implements UncaughtExceptionHandler {
